84bf2cc0a2a27f5aed305fd7d59bc0c8f23ee168
Mathaji <Mathajiphoshoko@gmail.com>
2025-12-06 12:47:26 +0200

    Hello.py with errors

--- hello_pre_commit.py
+++ hello_pre_commit.py
@@ -1,8 +1,12 @@
 def hello():
     name = input("Enter your name user: ")
-    print(f'Welcome to Pre-commit {name}  ')
+    print(f"Welcome to Pre-commit {name}  ")
+
 
 def main():
     hello()
+
+
 if __name__ == "__main__":
+
     main()
