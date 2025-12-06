3f3f5464419363d32896691115280641643816c4
Mathaji <Mathajiphoshoko@gmail.com>
2025-12-06 13:49:51 +0200

    Fixed versions for linters

--- hello_pre_commit.py
+++ hello_pre_commit.py
@@ -1,11 +1,15 @@
 def hello():
     name = input("Enter your name user: ")
-    print(f"Welcome to Pre-commit {name}  ")    
+    print(f'Welcome to Pre-commit {name}  ')   
+
+
+
 
 def main():
-      hello()
 
 
-if __name__ == "__main__":
+            hello()
 
+
+if __name__ == "__main__":  
     main()
