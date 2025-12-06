e27dca4726afd705e28548897605cdeabe658f43
Mathaji <Mathajiphoshoko@gmail.com>
2025-12-06 15:54:14 +0200

    Improved pylint formatting

---
+++ steps/hello_pre_commit.py
@@ -0,0 +1,14 @@
+def hello():
+    name = input("Enter your name user: ")
+    print(
+        f"Welcome to Pre-commit {name}   oinvibvhjvkjnsdajkvbuvbudbvbavibdivbidubviBvibajhvbivbaibvnvipvnivhonvbuahvabvuddkvjbiubvSLHbvuiSBvlsbvisdsvbsivbssbvhsdsivbdsuivdbviubvibvbsdljhbdidvbsdbvidbvo"
+    )
+
+
+def main():
+
+    hello()
+
+
+if __name__ == "__main__":
+    main()
